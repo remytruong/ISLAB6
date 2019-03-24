8cbf5a5adaca1746d1e0c6ae74df3b42a386b905
remytruong <remtruong@live.ca>
2019-03-24 13:50:49 -0700

    added unorderlist linking to assign function

---
+++ app/Views/chores.php
@@ -0,0 +1,13 @@
+<h2><?= $title ?></h2>
+<?php if( !empty($dwarves)) : ?>
+    <ul>
+    <?php foreach ($dwarves as $dwarf) : ?>
+        <li><a href="<?= \App\Controllers\Chores::assign($dwarf['id'])?>"><?= $dwarf['name'] ?></a></li>
+    <?php endforeach; ?>
+    </ul>
+<?php else : ?>
+
+    <h3>No Dwarves</h3>
+
+    <p>Unable to find any dwarves for you.</p>
+<?php endif ?>
